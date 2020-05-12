2bc3eda612981590a5846d1180d3780070dc0a4e
Dinis <dinispeixoto96@gmail.com>
2020-05-12 22:04:26 +0100

    raindrops

---
+++ raindrops/raindrops.py
@@ -0,0 +1,6 @@
+def convert(number):
+    result = ''
+    result += 'Pling' if number % 3 == 0 else ''
+    result += 'Plang' if number % 5 == 0 else ''
+    result += 'Plong' if number % 7 == 0 else ''
+    return result or str(number)
