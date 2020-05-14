0b2b99e82c794db312f45582760189c8d3b3aeb9
Dinis <dinispeixoto96@gmail.com>
2020-05-14 22:21:34 +0100

    hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,9 @@
 import typing
 
+
 def distance(strand_a: str, strand_b: str) -> int:
 
     if len(strand_a) != len(strand_b):
         raise ValueError("Different sizes")
 
-    return sum( a != b for a,b in zip(strand_a, strand_b))
+    return sum(a != b for a, b in zip(strand_a, strand_b))
