9feb4a3b08d95ee211c4721c5f288cee23798fb6
Dinis <dinispeixoto96@gmail.com>
2020-05-14 23:21:25 +0100

    word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,13 @@
+import re
+from itertools import groupby
+
+
 def count_words(sentence):
-    pass
+    sentence = [
+        x.strip("'") for x in re.split("[^a-z0-9']+", sentence.lower()) if x
+    ]
+
+    return {
+        word: len(list(group))
+        for word, group in groupby(sorted(sentence))
+    }
