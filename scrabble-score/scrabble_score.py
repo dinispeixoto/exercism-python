613b84800d6508ade50e169b4f3058867f0b822e
Dinis <dinispeixoto96@gmail.com>
2020-05-15 23:31:56 +0100

    scrabble-score

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,14 +1,14 @@
 
 scores = {
-    1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
-    2: ['d', 'g']
-    3: ['b', 'c', 'm', 'p']
-    4: ['f', 'h', 'v', 'w', 'y']
-    5: ['k']
-    8: ['j', 'x']
+    1: ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+    2: ['d', 'g'],
+    3: ['b', 'c', 'm', 'p'],
+    4: ['f', 'h', 'v', 'w', 'y'],
+    5: ['k'],
+    8: ['j', 'x'],
     10: ['q', 'z']
 }
 
 
 def score(word):
-    return sum([list(filter(lambda key: letter in scores[key], scores.key))[0] for letter in word])
+    return sum(sum([list(filter(lambda key: letter in scores[key], scores.keys())) for letter in word.lower()], []))
