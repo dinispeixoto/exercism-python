1b7a996eee1f45d1a269dfecf9edc0f149f8a85b
Dinis <dinispeixoto96@gmail.com>
2020-05-27 23:17:25 +0100

    kindergarten garden

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,4 +1,7 @@
-DEFAULT_STUDENTS = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+DEFAULT_STUDENTS = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet",
+    "Ileana", "Joseph", "Kincaid", "Larry"
+]
 
 PLANTS = {
     "G": "Grass",
@@ -9,7 +12,7 @@ PLANTS = {
 
 
 class Garden:
-    def __init__(self, diagram, students = DEFAULT_STUDENTS):
+    def __init__(self, diagram, students=DEFAULT_STUDENTS):
         self.studens = sorted(students)
         self.garden = diagram.split("\n")
 
@@ -23,17 +26,3 @@ class Garden:
             PLANTS[self.garden[1][first]],
             PLANTS[self.garden[1][second]],
         ]
-
-    # def _generate_plants(self, diagram, students):
-    #     plants = diagram.split("\n")
-
-    #     index = 0
-    #     garden = {}
-
-    #     for (plant_a, plant_b) in zip(*plants):
-    #         students[index] = [plant_a, plant_b]    
-
-    #     return garden
-
-
-
