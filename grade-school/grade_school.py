a89a8068172633ab2213d8addde7a8ae0da751d8
Dinis <dinispeixoto96@gmail.com>
2020-05-27 23:39:11 +0100

    grade school

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,15 @@
 class School:
     def __init__(self):
-        pass
+        self._roster = {}
 
     def add_student(self, name, grade):
-        pass
+        self._roster[grade] = self._roster.get(grade, []) + [name]
 
     def roster(self):
-        pass
+        return [
+            element for key in sorted(self._roster.keys())
+            for element in sorted(self._roster[key])
+        ]
 
     def grade(self, grade_number):
-        pass
+        return sorted(self._roster.get(grade_number, []))
