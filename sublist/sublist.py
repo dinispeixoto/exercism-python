ff3a7b0d2eb9fd768394c4f493b88ff28fa0b88f
Dinis <dinispeixoto96@gmail.com>
2020-06-10 22:44:44 +0100

    sublist

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -4,12 +4,20 @@ EQUAL = 3
 UNEQUAL = 4
 
 
+def tuples(list):
+    return [(list[i], list[i + 1]) for i in range(len(list) - 1)]
+
+
+def is_sublist(list_one, list_two):
+    return all(item in list_two for item in list_one) and all(item in tuples(list_two) for item in tuples(list_one))
+
+
 def sublist(list_one, list_two):
     if list_one == list_two:
         return EQUAL
-    elif all(item in list_two for item in list_one):
+    elif is_sublist(list_one, list_two):
         return SUBLIST
-    elif all(item in list_one for item in list_two):
+    elif is_sublist(list_two, list_one):
         return SUPERLIST
     else:
         return UNEQUAL
